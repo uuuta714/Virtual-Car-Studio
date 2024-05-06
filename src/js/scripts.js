57dada26a9d0ee60a4f732c096999fa6de218419
uuuta714 <horiyuuu@gmail.com>
2024-05-06 14:26:12 +1000

    Added video creation features including recording and exporting

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,41 +1,95 @@
 import * as THREE from 'three';
-import { Vector3, Euler } from 'three'
-import gsap from 'gsap';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
-import {RGBELoader} from 'three/examples/jsm/loaders/RGBELoader.js';
-
+import { Vector3 } from 'three'
+import { gsap } from 'gsap';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { TransformControls } from 'three/examples/jsm/controls/TransformControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import { CCapture } from 'ccapture.js-npmfixed';
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+const scene = new THREE.Scene();
 
 // Sets the color of the background
 renderer.setClearColor(0xFEFEFE);
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
+
+// Create Main Camera
+export const camera = new THREE.PerspectiveCamera(
     45,
     window.innerWidth / window.innerHeight,
-    0.1,
-    100
+    0.01,
+    50
 );
 
-// Sets orbit control to move the camera around
-const orbit = new OrbitControls(camera, renderer.domElement);
+// Orbit Controls
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.panSpeed = 1;
+orbitControls.rotateSpeed = 1;
+orbitControls.maxPolarAngle = Math.PI / 2;
 
-// Camera positioning
+// Configure main Camera
 camera.position.set(3, 3, 3);
-camera.lookAt(0,0,0);
-orbit.update();
+camera.lookAt(scene.position);
+orbitControls.update();
 
-// Sets a 12 by 12 gird helper
-const gridHelper = new THREE.GridHelper(12, 12);
-scene.add(gridHelper);
+// Create Custom Camera
+export const customCamera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    15
+);
 
-// Sets the x, y, and z axes with each having a length of 4
-const axesHelper = new THREE.AxesHelper(4);
-scene.add(axesHelper);
+// Configure custom camera in the scene
+customCamera.position.set(3, 1, 0);
+customCamera.lookAt(0, 0, 0);
+scene.add(customCamera);
+customCamera.updateMatrixWorld();
+
+// Custom camera helper
+const customCameraHelper = new THREE.CameraHelper( customCamera );
+scene.add( customCameraHelper );
+
+// Transform Controls
+const transformControls = new TransformControls(camera, renderer.domElement);
+transformControls.attach(customCamera);
+transformControls.setMode('rotate');
+scene.add(transformControls);
+
+transformControls.addEventListener('dragging-changed', function (event) {
+    orbitControls.enabled = !event.value
+});
+
+window.addEventListener('keydown', function (event) {
+    switch (event.key) {
+        case 't':
+            transformControls.setMode('translate')
+            break
+        case 'r':
+            transformControls.setMode('rotate')
+            break
+    }
+});
+
+
+// Configure CCapture for recording
+const capturer = new CCapture({
+    format: 'webm',
+    framerate: 60,
+    verbose: true
+});
+
+// // Sets a 12 by 12 gird helper
+// const gridHelper = new THREE.GridHelper(12, 12);
+// scene.add(gridHelper);
+
+// // Sets the x, y, and z axes with each having a length of 4
+// const axesHelper = new THREE.AxesHelper(4);
+// scene.add(axesHelper);
 
 const gltfLoader = new GLTFLoader();
 const rgbeLoader = new RGBELoader();
@@ -51,91 +105,61 @@ rgbeLoader.load('./assets/MR_INT-001_NaturalStudio_NAD.hdr', function(texture) {
 
     gltfLoader.load('./assets/scene.gltf', function(gltf) {
         const model = gltf.scene;
-        // model.traverse(function (child) {
-        //     if (child.isMesh) {
-        //         if (child.name == 'LightBulb') {
-        //             console.log('found bulb! ' + child.userData.name)
-        //             const pointLight = new THREE.PointLight( new THREE.Color(255,218,185), 0.005 );
-        //             child.add( pointLight );
-        //           }
-        //       child.name = "";
-        //       child.castShadow = true;
-        //     }
-        //   });
         scene.add(model);
         model.position.y += 0.65;
         car = model;
     });
 });
 
-
 function animate() {
+    requestAnimationFrame(animate);
     renderer.render(scene, camera);
+    capturer.capture(renderer.domElement);
 }
-
-renderer.setAnimationLoop(animate);
+animate();
 
 window.addEventListener('resize', function() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    //renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
 
 
 // Camera Controls
 
-// Array storing all options of camera sequence
+// Array storing all options of camera movement
 export const cameraSequenceOptions = [
     {
         id: 1,
-        name: 'Front View',
-        startPosition: new Vector3(0.0, 0.5, 3.0), 
-        lookAt: new Vector3(0.0, 0.0, 0.0),
-        endPosition: new Vector3(0.0, 1.0, 3.0),
-        rotation: null,
-        duration: 7.0
-    },
-    {
-        id: 2,
-        name: 'Left Front Light View',
-        startPosition: new Vector3(0.7, 0.7, 2.8),
-        lookAt: new Vector3(0.0, 0.0, 0.0),
-        endPosition: new Vector3(1.3, 0.7, 2.4),
-        rotation: new Euler(0.0, 45 * Math.PI / 180, 0.0),
-        duration: 7.0
-    },
-    {
-        id: 3,
-        name: 'Left Front Tyre View',
-        startPosition: new Vector3(1.9, 0.6, 1.3),
-        lookAt: new Vector3(0.0, 0.0, 1.3),
-        endPosition: new Vector3(2.5, 1.0, 1.3),
-        rotation: null,
-        duration: 7.0
-    },
-    {
-        id: 4,
-        name: 'Side View from Top',
-        startPosition: new Vector3(3.5, 2.2, 1.6),
-        lookAt: new Vector3(0.0, 0.0, 1.6),
-        endPosition: new Vector3(3.5, 2.2, -1,3),
-        rotation: null,
-        duration: 7.0
-    },
-    {
-        id: 5,
-        name: 'Porsche Logo View Back',
-        startPosition: new Vector3(0.23, 0.73, -2.4),
-        lookAt: new Vector3(0.0, 0.73, 0.0),
-        endPosition: new Vector3(-0.23, 0.73, -2.4),
-        rotation: null,
+        name: 'Test',
+        startCameraPosition: new Vector3(1.49, 1.16, -3.20),
+        startLookAtPosition: new Vector3(-49.83, -29.07, 77.12),
+        endCameraPosition: new Vector3(-0.30, 4.74, -0.28),
+        endLookAtPosition: new Vector3(-0.30, -95.26, -0.28),
         duration: 7.0
     },
 ]
 
 // Array to store selected camera sequence
 export let selectedCameraSequence = [];
+
+// Function to add a new camera movement to cameraSequenceOptions
+export function createCameraMovement(name, positions, duration) {
+    const { startPosition, startLookAt, endPosition, endLookAt } = positions;
+    const newCameraMovement = {
+        id: cameraSequenceOptions.length + 1,
+        name,
+        startCameraPosition: new Vector3(startPosition.x, startPosition.y, startPosition.z),
+        startLookAtPosition: new Vector3(startLookAt.x, startLookAt.y, startLookAt.z),
+        endCameraPosition: new Vector3(endPosition.x, endPosition.y, endPosition.z),
+        endLookAtPosition: new Vector3(endLookAt.x, endLookAt.y, endLookAt.z),
+        duration
+    };
+
+    cameraSequenceOptions.push(newCameraMovement);
+    console.log('Camera movement created:', newCameraMovement);
+}
 
 // Function to add selected camera sequence to selectedCameraSequence array
 export function addCameraSequenceById(sequenceId) {
@@ -147,40 +171,56 @@ export function addCameraSequenceById(sequenceId) {
 
 const tl = gsap.timeline();
 
-
 // Function to iterate selectedCameraSequence array to move camera from one sequence to another
 export function startCameraSequence() {
+    
+    const totalDuration = selectedCameraSequence.reduce((acc, item) => acc + item.duration, 0);
+    console.log(totalDuration);
+    console.log(renderer.info.render.frame);
+    
     tl.clear();
+
+    // Start recording
+    capturer.start();
 
     selectedCameraSequence.forEach(sequence => {
         tl.add(() => {
-            camera.position.copy(sequence.startPosition);
-            camera.lookAt(sequence.lookAt.x, sequence.lookAt.y, sequence.lookAt.z);
+            camera.position.copy(sequence.startCameraPosition);
+            camera.lookAt(sequence.startLookAtPosition.x, sequence.startLookAtPosition.y, sequence.startLookAtPosition.z);
         });
 
         tl.to(camera.position, {
-            x: sequence.endPosition.x,
-            y: sequence.endPosition.y,
-            z: sequence.endPosition.z,
+            x: sequence.endCameraPosition.x,
+            y: sequence.endCameraPosition.y,
+            z: sequence.endCameraPosition.z,
             duration: sequence.duration,
             ease: "none"
         });
 
-        if (sequence.rotation) {
-            tl.to(camera.rotation, {
-                x: sequence.rotation.x,
-                y: sequence.rotation.y,
-                z: sequence.rotation.z,
-                duration: sequence.duration,
-                ease: "none"
-            }, "<");
-        }
+        // Animate camera's direction by interpolating between startLookAtPosition and endLookAtPosition
+        tl.to({}, {
+            duration: sequence.duration,
+            ease: "none",
+            onUpdate: function() {
+                const progress = this.progress();
+                const directionX = gsap.utils.interpolate(sequence.startLookAtPosition.x, sequence.endLookAtPosition.x, progress);
+                const directionY = gsap.utils.interpolate(sequence.startLookAtPosition.y, sequence.endLookAtPosition.y, progress);
+                const directionZ = gsap.utils.interpolate(sequence.startLookAtPosition.z, sequence.endLookAtPosition.z, progress);
+                camera.lookAt(new THREE.Vector3(directionX, directionY, directionZ));
+            }
+        }, "<");
     });
 
-    // Set the camera back to default position
-    tl.add(() => {
+    // Stop recording when the timeline is complete
+    tl.eventCallback("onComplete", function() {
+        console.log(renderer.info.render.frame);
+        capturer.stop();
+        capturer.save();  // This triggers the download of the recorded video
+
+        // Set the camera and view to original state
         camera.position.set(3, 3, 3);
         camera.lookAt(0, 0, 0);
+        resetView();
     });
 }
 
@@ -189,3 +229,63 @@ export function resetCameraSequence() {
     selectedCameraSequence = [];
 }
 
+
+// Function to return current camera position and lookAt position
+export function getCameraDetails(cam) {
+    // Get the position of the camera
+    const position = cam.position.clone();
+    
+    // Get the direction the camera is looking at
+    const direction = new THREE.Vector3();
+    cam.getWorldDirection(direction);
+    
+    // Calculate a point in the direction the camera is facing
+    const lookAtPosition = direction.multiplyScalar(100).add(cam.position);
+
+    return {
+        position: {
+            x: position.x,
+            y: position.y,
+            z: position.z
+        },
+        lookAtPosition: {
+            x: lookAtPosition.x,
+            y: lookAtPosition.y,
+            z: lookAtPosition.z
+        }
+    };
+}
+
+
+
+// GUI Control
+var gui = new GUI();
+
+// Function to toggle GUI visibility
+function toggleGUIVisibility() {
+    gui.domElement.style.display = (gui.domElement.style.display === 'none' ? '' : 'none');
+}
+
+// Parameters of GUI
+var params = {
+    showCustomCameraHelper: true,
+};
+
+// Add a toggle in the GUI
+gui.add(params, 'showCustomCameraHelper').name('Show Custom Camera').onChange(value => {
+    customCameraHelper.visible = value;
+});
+
+// Function to clean window for video recording
+export function cleanView() {
+    customCameraHelper.visible = false;
+    toggleGUIVisibility();
+    transformControls.visible = false;
+}
+
+// Function to reset window after video recording
+function resetView() {
+    customCameraHelper.visible = true;
+    toggleGUIVisibility();
+    transformControls.visible = true;
+}
--- src/js/userInteractions.js
+++ src/js/userInteractions.js
@@ -1,4 +1,14 @@
-import { cameraSequenceOptions, addCameraSequenceById, selectedCameraSequence, startCameraSequence, resetCameraSequence } from './scripts.js';
+import {
+    camera,
+    customCamera,
+    cameraSequenceOptions,
+    addCameraSequenceById,
+    selectedCameraSequence,
+    startCameraSequence,
+    resetCameraSequence,
+    getCameraDetails,
+    createCameraMovement,
+    cleanView } from './scripts.js';
 
 
 // Event listener to open the side modal
@@ -13,24 +23,130 @@ document.getElementById('close-slideout').addEventListener('click', function(eve
     document.getElementById('slideout').classList.remove('open');
 });
 
+// Get camera start position and start lookAt position from main camera (browser view)
+document.getElementById('setStartPositionFromMainCamera').addEventListener('click', function() {
+    const cameraDetails = getCameraDetails(camera);
+    console.log('Camera Position:', cameraDetails.position);
+    console.log('LookAt Position:', cameraDetails.lookAtPosition);
+
+    // Set the values of the input fields for camera position
+    document.getElementById('cameraStartPositionX').value = cameraDetails.position.x.toFixed(2);
+    document.getElementById('cameraStartPositionY').value = cameraDetails.position.y.toFixed(2);
+    document.getElementById('cameraStartPositionZ').value = cameraDetails.position.z.toFixed(2);
+
+    // Set the values of the input fields for camera lookAt position
+    document.getElementById('cameraStartLookAtPositionX').value = cameraDetails.lookAtPosition.x.toFixed(2);
+    document.getElementById('cameraStartLookAtPositionY').value = cameraDetails.lookAtPosition.y.toFixed(2);
+    document.getElementById('cameraStartLookAtPositionZ').value = cameraDetails.lookAtPosition.z.toFixed(2);
+});
+
+// Get camera end position and end lookAt position from main camera (browser view)
+document.getElementById('setEndPositionFromMainCamera').addEventListener('click', function() {
+    const cameraDetails = getCameraDetails(camera);
+    console.log('Camera Position:', cameraDetails.position);
+    console.log('LookAt Position:', cameraDetails.lookAtPosition);
+
+    // Set the values of the input fields for camera position
+    document.getElementById('cameraEndPositionX').value = cameraDetails.position.x.toFixed(2);
+    document.getElementById('cameraEndPositionY').value = cameraDetails.position.y.toFixed(2);
+    document.getElementById('cameraEndPositionZ').value = cameraDetails.position.z.toFixed(2);
+
+    // Set the values of the input fields for camera lookAt position
+    document.getElementById('cameraEndLookAtPositionX').value = cameraDetails.lookAtPosition.x.toFixed(2);
+    document.getElementById('cameraEndLookAtPositionY').value = cameraDetails.lookAtPosition.y.toFixed(2);
+    document.getElementById('cameraEndLookAtPositionZ').value = cameraDetails.lookAtPosition.z.toFixed(2);
+});
+
+// Get camera start position and start lookAt position from custom camera
+document.getElementById('setStartPositionFromCustomCamera').addEventListener('click', function() {
+    const cameraDetails = getCameraDetails(customCamera);
+    console.log('Camera Position:', cameraDetails.position);
+    console.log('LookAt Position:', cameraDetails.lookAtPosition);
+
+    // Set the values of the input fields for camera position
+    document.getElementById('cameraStartPositionX').value = cameraDetails.position.x.toFixed(2);
+    document.getElementById('cameraStartPositionY').value = cameraDetails.position.y.toFixed(2);
+    document.getElementById('cameraStartPositionZ').value = cameraDetails.position.z.toFixed(2);
+
+    // Set the values of the input fields for camera lookAt position
+    document.getElementById('cameraStartLookAtPositionX').value = cameraDetails.lookAtPosition.x.toFixed(2);
+    document.getElementById('cameraStartLookAtPositionY').value = cameraDetails.lookAtPosition.y.toFixed(2);
+    document.getElementById('cameraStartLookAtPositionZ').value = cameraDetails.lookAtPosition.z.toFixed(2);
+});
+
+// Get camera end position and end lookAt position from custom camera
+document.getElementById('setEndPositionFromCustomCamera').addEventListener('click', function() {
+    const cameraDetails = getCameraDetails(customCamera);
+    console.log('Camera Position:', cameraDetails.position);
+    console.log('LookAt Position:', cameraDetails.lookAtPosition);
+
+    // Set the values of the input fields for camera position
+    document.getElementById('cameraEndPositionX').value = cameraDetails.position.x.toFixed(2);
+    document.getElementById('cameraEndPositionY').value = cameraDetails.position.y.toFixed(2);
+    document.getElementById('cameraEndPositionZ').value = cameraDetails.position.z.toFixed(2);
+
+    // Set the values of the input fields for camera lookAt position
+    document.getElementById('cameraEndLookAtPositionX').value = cameraDetails.lookAtPosition.x.toFixed(2);
+    document.getElementById('cameraEndLookAtPositionY').value = cameraDetails.lookAtPosition.y.toFixed(2);
+    document.getElementById('cameraEndLookAtPositionZ').value = cameraDetails.lookAtPosition.z.toFixed(2);
+});
+
+// Add created camera movement to cameraSequenceOptions
+document.getElementById('createCameraMovement').addEventListener('click', function() {
+    const name = document.getElementById('cameraMovementName').value;
+    const duration = parseFloat(document.getElementById('duration').value);
+    const positions = {
+        startPosition: {
+            x: parseFloat(document.getElementById('cameraStartPositionX').value),
+            y: parseFloat(document.getElementById('cameraStartPositionY').value),
+            z: parseFloat(document.getElementById('cameraStartPositionZ').value)
+        },
+        startLookAt: {
+            x: parseFloat(document.getElementById('cameraStartLookAtPositionX').value),
+            y: parseFloat(document.getElementById('cameraStartLookAtPositionY').value),
+            z: parseFloat(document.getElementById('cameraStartLookAtPositionZ').value)
+        },
+        endPosition: {
+            x: parseFloat(document.getElementById('cameraEndPositionX').value),
+            y: parseFloat(document.getElementById('cameraEndPositionY').value),
+            z: parseFloat(document.getElementById('cameraEndPositionZ').value)
+        },
+        endLookAt: {
+            x: parseFloat(document.getElementById('cameraEndLookAtPositionX').value),
+            y: parseFloat(document.getElementById('cameraEndLookAtPositionY').value),
+            z: parseFloat(document.getElementById('cameraEndLookAtPositionZ').value)
+        }
+    };
+
+    // Call the function to add a new camera movement
+    createCameraMovement(name, positions, duration);
+    updateCameraSequenceDropdown();
+});
+
 // Start camera movements based on the selected camera sequence
 document.getElementById('start-camera-sequence').addEventListener('click', function(event) {
     event.preventDefault();
-    startCameraSequence();
+    cleanView();
     document.getElementById('slideout').classList.remove('open');
+    startCameraSequence();
 });
 
-// Populate the dropdown menu when the slideout opens
-document.getElementById('open-slideout').addEventListener('click', function() {
+// Function to populate the dropdown menu
+function updateCameraSequenceDropdown() {
     const dropdown = document.getElementById('cameraSequenceDropdown');
     dropdown.innerHTML = '<option value="">Select a Camera Sequence</option>'; // Clear previous options
 
     cameraSequenceOptions.forEach(option => {
         const optionElement = document.createElement('option');
         optionElement.value = option.id;
-        optionElement.textContent = `${option.name}`;
+        optionElement.textContent = option.name;
         dropdown.appendChild(optionElement);
     });
+}
+
+// Populate the dropdown menu when the slideout opens
+document.getElementById('open-slideout').addEventListener('click', function() {
+    updateCameraSequenceDropdown();
 });
 
 // Function to update the display of selected sequences
