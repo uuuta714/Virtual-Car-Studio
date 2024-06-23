0703a0dbd15dfbc5675ce49b548ab760cb1c40b6
uuuta714 <horiyuuu@gmail.com>
2024-06-23 16:09:22 +1000

    Modified CameraMovement data structure to hold ease information and related Javascript codes

--- src/js/userInteractions.js
+++ src/js/userInteractions.js
@@ -10,9 +10,8 @@ import {
     createCameraMovement,
     cleanView,
     enableRecording,
-    displayCustomCamera,
-    objects,
-    selectedObject } from './scripts.js';
+    displayCustomCamera
+    } from './scripts.js';
 
 import {
     updateStudioLightDropdown
@@ -23,25 +22,6 @@ import * as bootstrap from 'bootstrap';
 const offcanvasElement = document.getElementById('offcanvasNavbar');
 // Create a new Offcanvas instance
 const bsOffcanvas = new bootstrap.Offcanvas(offcanvasElement);
-
-// To open the offcanvas
-// bsOffcanvas.show();
-
-// To close the offcanvas
-// bsOffcanvas.hide();
-
-
-// // Event listener to open the side modal
-// document.getElementById('open-slideout').addEventListener('click', function(event) {
-//     event.preventDefault();
-//     document.getElementById('slideout').classList.add('open');
-// });
-
-// // Event listener to close the side modal
-// document.getElementById('close-slideout').addEventListener('click', function(event) {
-//     event.preventDefault();
-//     document.getElementById('slideout').classList.remove('open');
-// });
 
 // Toggle visibility of the custom camera
 document.getElementById('custom-camera-visibility').addEventListener('change', function() {
@@ -120,6 +100,10 @@ document.getElementById('setEndPositionFromCustomCamera').addEventListener('clic
 document.getElementById('createCameraMovement').addEventListener('click', function() {
     const name = document.getElementById('cameraMovementName').value;
     const duration = parseFloat(document.getElementById('duration').value);
+    let ease = document.getElementById('easingEffectDropdown').value + '.' + document.getElementById('easingTypeDropdown').value;
+        if (document.getElementById('easingEffectDropdown').value == 'none') {
+            ease = 'none'
+        }
     const positions = {
         startPosition: {
             x: parseFloat(document.getElementById('cameraStartPositionX').value),
@@ -144,8 +128,19 @@ document.getElementById('createCameraMovement').addEventListener('click', functi
     };
 
     // Call the function to add a new camera movement
-    createCameraMovement(name, positions, duration);
+    createCameraMovement(name, positions, duration, ease);
     updateCameraSequenceDropdown();
+});
+
+// Disable easingTypeDropdown when 'none' is selected for easingEffect
+document.getElementById('easingEffectDropdown').addEventListener('change', function() {
+    var selectedEffect = this.value;
+    // Disable or enable the easingTypeDropdown based on the selected value
+    if (selectedEffect == 'none') {
+        document.getElementById('easingTypeDropdown').disabled = true;
+    } else {
+        document.getElementById('easingTypeDropdown').disabled = false;
+    }
 });
 
 // Start preview based on the selected camera sequence
@@ -177,11 +172,6 @@ function updateCameraSequenceDropdown() {
         dropdown.appendChild(optionElement);
     });
 }
-
-// // Populate the dropdown menu when the slideout opens
-// document.getElementById('open-slideout').addEventListener('click', function() {
-//     updateCameraSequenceDropdown();
-// });
 
 // Populate the dropdown menu when Offcanvas is about to show
 offcanvasElement.addEventListener('show.bs.offcanvas', function () {
@@ -229,21 +219,3 @@ document.getElementById('resetSequenceButton').addEventListener('click', () => {
     updateSequenceListDisplay(); // Update the list display after reset
     console.log('Selected Camera Sequences have been reset');
 });
-
-// // Event listener for opening lighting control slideout
-// document.getElementById("openButton").addEventListener("click", openSlideOut);
-// document.getElementById("closeButton").addEventListener("click", closeSlideOut);
-
-// // Control the lighting control slideout
-// export function openSlideOut() {
-//     document.getElementById("rightSlideout").style.right = "0";
-// }
-
-// export function closeSlideOut() {
-//     document.getElementById("rightSlideout").style.right = "-250px";
-// }
-
-
-// // Event listener for adding light isntance
-// document.getElementById("addLightButton").addEventListener("click", createLightInstance);
-
